fe3a07b2c5d833138f0a9ea8d4b80d0b6f7dbfe9
vinogradov.max <vinogradov.max97@gmail.com>
2016-06-06 21:19:30 +0300

    [#19] Post stab was added.

---
+++ modules/database/src/main/java/nc/sumy/edu/webapp/database/stubs/Post.java
@@ -0,0 +1,88 @@
+package nc.sumy.edu.webapp.database.stubs;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.Calendar;
+
+public class Post {
+    private long        postId;
+    private long        userId;
+    private Calendar    publishDate;
+    private String      title;
+    private String      body;
+    //TODO: write a conversion from Timestamp to Calendar if it will needed.
+
+    public long getPostId() {
+        return postId;
+    }
+
+    public Post setPostId(long postId) {
+        this.postId = postId;
+        return this;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public Post setUserId(long userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public Calendar getPublishDate() {
+        return publishDate;
+    }
+
+    public Post setPublishDate(Calendar publishDate) {
+        this.publishDate = publishDate;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Post setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Post setBody(String body) {
+        this.body = body;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Post post = (Post) obj;
+
+        return new EqualsBuilder()
+                .append(postId, post.postId)
+                .append(userId, post.userId)
+                .append(publishDate, post.publishDate)
+                .append(title, post.title)
+                .append(body, post.body)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(postId)
+                .append(userId)
+                .append(publishDate)
+                .append(title)
+                .append(body)
+                .toHashCode();
+    }
+}
