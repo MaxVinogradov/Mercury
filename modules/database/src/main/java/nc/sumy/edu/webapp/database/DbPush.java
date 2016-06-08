925020b13fe0b0f7ff4312ace42b089d960a49f9
vinogradov.max <vinogradov.max97@gmail.com>
2016-06-08 20:04:37 +0300

    [#19] DbPush and DbPushImpl were added.

--- modules/database/src/main/java/nc/sumy/edu/webapp/database/DbPush.java
+++ modules/database/src/main/java/nc/sumy/edu/webapp/database/DbPush.java
@@ -1,4 +1,18 @@
 package nc.sumy.edu.webapp.database;
 
+import nc.sumy.edu.webapp.database.stubs.Account;
+import nc.sumy.edu.webapp.database.stubs.Portal;
+import nc.sumy.edu.webapp.database.stubs.Post;
+import nc.sumy.edu.webapp.database.stubs.User;
+
 public interface DbPush {
+
+    void addUser(User user);
+
+    void addPost(Post post);
+
+    void addAccount(Account account);
+
+    void addPortal(Portal portal);
+
 }
