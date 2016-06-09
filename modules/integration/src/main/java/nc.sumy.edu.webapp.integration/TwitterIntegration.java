84af042d12e81e31b2d67e81e64852e54940c995
OlehLuk <vadlukcis@gmail.com>
2016-06-09 13:41:27 +0300

    [#9]  twitter integration implementation started

--- modules/integration/src/main/java/nc.sumy.edu.webapp.integration/TwitterIntegration.java
+++ modules/integration/src/main/java/nc.sumy.edu.webapp.integration/TwitterIntegration.java
@@ -1,14 +1,26 @@
 package nc.sumy.edu.webapp.integration;
 
 
+import com.github.scribejava.apis.TwitterApi;
+import com.github.scribejava.apis.VkontakteApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth1RequestToken;
 import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.oauth.OAuth10aService;
+import com.github.scribejava.core.oauth.OAuth20Service;
 
 public class TwitterIntegration implements SocialNetworkIntegration{
-
+    private static final String APP_ID = "6DCIfit5FkI6bbj0I9OUDb6IO";
+    private static final String APP_SECRET = "0Wd0RnccP4GVllbjhK9nREnTfPmx7TSGyGdYoYHUsNIhMNjggt";
+    private final OAuth10aService service = new ServiceBuilder()
+            .apiKey(APP_ID)
+            .apiSecret(APP_SECRET)
+            .callback("http://www.example.com/oauth_callback/")
+            .build(TwitterApi.instance());
 
     @Override
     public String getAuthorisationUrl() {
-        return null;
+        return service.getAuthorizationUrl(service.getRequestToken());
     }
 
     @Override
