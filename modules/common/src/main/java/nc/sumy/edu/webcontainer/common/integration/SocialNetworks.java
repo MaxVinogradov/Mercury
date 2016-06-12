4ebe018a5d61de789cacf3edb4ea3cf436589ed9
OlehLuk <vadlukcis@gmail.com>
2016-06-12 16:25:34 +0300

    [#9] enum for network names improved. Methods for string values added

--- modules/common/src/main/java/nc/sumy/edu/webcontainer/common/integration/SocialNetworks.java
+++ modules/common/src/main/java/nc/sumy/edu/webcontainer/common/integration/SocialNetworks.java
@@ -1,5 +1,26 @@
 package nc.sumy.edu.webcontainer.common.integration;
 
 public enum SocialNetworks {
-    VK, FACEBOOK, TWITTER
+    VK("vk.com"),
+    FACEBOOK("facebook.com"),
+    TWITTER("twitter.com");
+
+    private String databaseName;
+
+    SocialNetworks(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public static SocialNetworks getNetworkType(String name) {
+        for (SocialNetworks network: SocialNetworks.values()) {
+            if (network.databaseName.equals(name)) {
+                return network;
+            }
+        }
+        return null;
+    }
 }
