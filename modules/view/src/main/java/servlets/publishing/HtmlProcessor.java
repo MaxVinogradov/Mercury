3e78ee2d077e03f91a1076d6f5d1c96638658b2e
Vinogradov <vinogradov.max49@gmail.com>
2016-06-18 15:07:02 +0300

    Methods for creation post list was created.

--- modules/view/src/main/java/servlets/publishing/HtmlProcessor.java
+++ modules/view/src/main/java/servlets/publishing/HtmlProcessor.java
@@ -1,20 +1,29 @@
 package servlets.publishing;
 
+import nc.sumy.edu.webapp.orm.domain.Post;
+
+import java.util.Collection;
+
 public class HtmlProcessor {
     private static final String TD_OP = "<td>";
     private static final String TD_CL = "</td>";
     private static final String TR_OP = "<tr>";
     private static final String TR_CL = "</tr>";
-    private static final String SPAN_OP = "<span>";
     private static final String SPAN_OP_GI = "<span ";
     private static final String SPAN_CL = "</span>";
+    private static final String DIV_OP = "<div class=\"container\">";
+    private static final String DIV_CL = "</div>";
+    private static final String P_OP = "<p>";
+    private static final String P_CL = "</p>";
+    private static final String H3_OP = "<h3 class=\"text-center\">";
+    private static final String H3_CL = "</h3>";
 
     public String createTableRow(String... data) {
         StringBuilder builder = new StringBuilder();
         builder.append(TR_OP);
-        for (int i = 0; i < data.length; i++) {
+        for (String aData : data) {
             builder.append(TD_OP)
-                    .append(data)
+                    .append(aData)
                     .append(TD_CL);
         }
         builder.append(TR_CL);
@@ -25,12 +34,20 @@ public class HtmlProcessor {
         return SPAN_OP_GI + "class=\"glyphicon glyphicon-" + glyphiconType + "\">" + SPAN_CL;
     }
 
-    public String createPostList() {
-        return null;
+    public String createPostList(Collection<Post> posts) {
+        StringBuilder builder = new StringBuilder();
+        for (Post post: posts) {
+            builder.append(DIV_OP)
+                    .append(createPost(post))
+                    .append(DIV_CL);
+        }
+        return builder.toString();
     }
 
-    public String createPost() {
-        return null;
+    private String createPost(Post post) {
+        return H3_OP + "Mercury" + H3_CL +
+                P_OP + post.getBody() + P_CL +
+                P_OP + post.getPublishDate() + P_CL;
     }
 
 }
