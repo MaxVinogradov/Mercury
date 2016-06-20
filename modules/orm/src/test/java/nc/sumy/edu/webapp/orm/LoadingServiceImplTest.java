9687e8378820e109053a01c48aad906b073a98fd
AlexeyZhulyov <alexey.zhulyov@gmail.com>
2016-06-21 01:20:45 +0300

    [#15] Add first test for database

--- modules/orm/src/test/java/nc/sumy/edu/webapp/orm/LoadingServiceImplTest.java
+++ modules/orm/src/test/java/nc/sumy/edu/webapp/orm/LoadingServiceImplTest.java
@@ -1,16 +1,79 @@
 package nc.sumy.edu.webapp.orm;
 
+
+import nc.sumy.edu.webapp.database.DataBaseConnection;
+import nc.sumy.edu.webapp.database.DataBaseConnectionH2;
+import org.dbunit.Assertion;
 import org.dbunit.DBTestCase;
+import org.dbunit.DatabaseUnitException;
 import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.dbunit.PropertiesBasedJdbcDatabaseTester.*;
 
 public class LoadingServiceImplTest extends DBTestCase {
 
-    public LoadingServiceImplTest() {
-
-    }
+    private final DataBaseConnection dbCon = new DataBaseConnectionH2();
 
     @Override
-    protected IDataSet getDataSet() throws Exception {
-        return null;
+    protected IDataSet getDataSet() throws DatabaseUnitException, IOException {
+        return new FlatXmlDataSetBuilder().build(new FileInputStream("dataset.xml"));
+    }
+
+    public LoadingServiceImplTest(String name) {
+        super(name);
+        System.setProperty(DBUNIT_DRIVER_CLASS, "org.h2.Driver");
+        System.setProperty(DBUNIT_CONNECTION_URL, "jdbc:h2:~/test");
+        System.setProperty(DBUNIT_USERNAME, "sa");
+        System.setProperty(DBUNIT_PASSWORD, "");
+        System.setProperty(DBUNIT_SCHEMA, "PUBLIC");
+    }
+
+    @Test
+    public void testUsers() throws Exception {
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable("USERS");
+
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("dataset.xml"));
+        ITable expectedTable = expectedDataSet.getTable("USERS");
+
+        Assertion.assertEquals(expectedTable, actualTable);
+    }
+
+    @Test
+    public void testAccounts() throws Exception {
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable("ACCOUNTS");
+
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("dataset.xml"));
+        ITable expectedTable = expectedDataSet.getTable("ACCOUNTS");
+
+        Assertion.assertEquals(expectedTable, actualTable);
+    }
+
+    @Test
+    public void testPortals() throws Exception {
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable("PORTALS");
+
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("dataset.xml"));
+        ITable expectedTable = expectedDataSet.getTable("PORTALS");
+
+        Assertion.assertEquals(expectedTable, actualTable);
+    }
+
+    @Test
+    public void testPosts() throws Exception {
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable("POSTS");
+
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("dataset.xml"));
+        ITable expectedTable = expectedDataSet.getTable("POSTS");
+
+        Assertion.assertEquals(expectedTable, actualTable);
     }
 }
