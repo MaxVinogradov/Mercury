9c799951af504040fbee26dbdc25666c0e894769
vinogradov.max <vinogradov.max97@gmail.com>
2016-06-20 10:57:32 +0300

    First parameter was added.

--- modules/view/src/main/java/nc/sumy/edu/webapp/enums/FormParameters.java
+++ modules/view/src/main/java/nc/sumy/edu/webapp/enums/FormParameters.java
@@ -1,4 +1,16 @@
 package nc.sumy.edu.webapp.enums;
 
 public enum FormParameters {
+    LOG_IN_PARAM("login");
+
+    private final String parameter;
+
+    FormParameters(String parameter) {
+        this.parameter = parameter;
+    }
+
+    @Override
+    public String toString() {
+        return this.parameter;
+    }
 }
