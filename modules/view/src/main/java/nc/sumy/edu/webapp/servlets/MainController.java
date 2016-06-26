1639fae1242ae6396f60cea719049c59ab90e307
vinogradov.max <vinogradov.max97@gmail.com>
2016-06-26 12:27:17 +0300

    Unique Constraint in table accounts on columns service_name and login was removed.

--- modules/view/src/main/java/nc/sumy/edu/webapp/servlets/MainController.java
+++ modules/view/src/main/java/nc/sumy/edu/webapp/servlets/MainController.java
@@ -44,8 +44,8 @@ public class MainController extends HttpServlet {
                 break;
                 case ("isSessionExist") : {
                     if (nonNull(request.getSession(false))) {
-                        response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
-                        response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
+                        response.setContentType("text/plain");
+                        response.setCharacterEncoding("UTF-8");
                         response.getWriter().write("exist");
                     }
                 }
